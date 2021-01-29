458571652488efe0f5e8d1b5afeeecbb83f4cceb
dengquanliang <dengquanliang@kuaishou.com>
2021-01-29 19:04:00 +0800

    add

--- src/main/java/com/bupt/dql/web/LoginLogConstant.java
+++ src/main/java/com/bupt/dql/web/LoginLogConstant.java
@@ -7,4 +7,10 @@ package com.bupt.dql.web;
 public class LoginLogConstant {
     public static final String LOGIN = "登陆系统";
     public static final String LOGOUT = "退出系统";
+    public static final String ADD_USER = "添加系统人员";
+    public static final String DELETE_USER = "删除系统人员";
+    public static final String UPDATE_USER = "修改系统人员";
+    public static final String ADD_WORD = "添加敏感词";
+    public static final String DELETE_WORD = "删除敏感词";
+    public static final String UPDATE_WORD = "修改敏感词";
 }
---
+++ src/main/java/com/bupt/dql/web/Test.java
@@ -0,0 +1,24 @@
+package com.bupt.dql.web;
+
+import static com.bupt.dql.web.LoginLogConstant.ADD_USER;
+import static com.bupt.dql.web.LoginLogConstant.ADD_WORD;
+import static com.bupt.dql.web.LoginLogConstant.DELETE_USER;
+import static com.bupt.dql.web.LoginLogConstant.DELETE_WORD;
+import static com.bupt.dql.web.LoginLogConstant.UPDATE_USER;
+import static com.bupt.dql.web.LoginLogConstant.UPDATE_WORD;
+
+/**
+ * @author dengquanliang
+ * Created on 2021/1/29
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String a = ADD_USER;
+        String b = ADD_WORD;
+        String c = UPDATE_USER;
+        String d = UPDATE_WORD;
+        String a1 = DELETE_USER;
+        String b1 = DELETE_WORD;
+    }
+}
